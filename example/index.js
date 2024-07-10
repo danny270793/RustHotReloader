0ed2c6e0ee0a023f9b26fbf2eebc72dee460b1c7
Danny Vaca <danny270793@gmail.com>
2024-07-10 18:10:18 -0500

    add more files

--- example/index.js
+++ example/index.js
@@ -1,3 +1,4 @@
 const index2 = require('./index2')
+const index3 = require('./index3')
 
 console.log('hola mundo from index.js')
---
+++ example/index3.js
@@ -0,0 +1,3 @@
+console.log('hello world from index3.js')
+
+exports.default = {}
